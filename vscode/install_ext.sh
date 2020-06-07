246e0131797fadbfd71c89f3c3c333907b353776
Alex <bloodhound1982@gmail.com>
2020-06-07 13:36:34 +0300

    Update install_ext.sh

--- vscode/install_ext.sh
+++ vscode/install_ext.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
 cat vscode.ext.txt | xargs -L 1 code --install-extension
+cp keybindings.json ~/Library/Application\ Support/Code/User/
+cp settings.json ~/Library/Application\ Support/Code/User/
+cp snippets/* ~/Library/Application\ Support/Code/User/snippents/* 
