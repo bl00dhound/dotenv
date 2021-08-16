ffaee7fca1d942b33c1fdc91ffb73d1f49c9371d
Oleksii Kuznietsov <oleksii.kuznietsov@alteryx.com>
2021-08-16 11:33:24 +0300

    Update: new configs are added

--- vscode/install_ext.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-cat vscode.ext.txt | xargs -L 1 code --install-extension
-cp keybindings.json ~/Library/Application\ Support/Code/User/
-cp settings.json ~/Library/Application\ Support/Code/User/
-cp snippets/* ~/Library/Application\ Support/Code/User/snippets/
